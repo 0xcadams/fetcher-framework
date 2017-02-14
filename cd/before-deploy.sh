364d4706e12ea68c7dc6d4ce3cd22c9341261d75
Chase Adams <Chase.Adams@infor.com>
2017-02-14 14:45:02 -0700

    modified travis to build and deploy from master branch

---
+++ cd/before-deploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    openssl aes-256-cbc -K $encrypted_7786718ab80b_key -iv $encrypted_7786718ab80b_iv -in signingkey.asc.enc -out signingkey.asc -d
+    gpg --fast-import cd/signingkey.asc
+fi
