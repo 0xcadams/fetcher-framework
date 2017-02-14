3d21aa7a5e86f2de40fd8fb1d223a5eba417fd16
Chase Adams <Chase.Adams@infor.com>
2017-02-14 16:14:59 -0700

    changed signing key

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_7786718ab80b_key -iv $encrypted_7786718ab80b_iv -in cd/signingkey.asc.enc -out cd/signingkey.asc -d
-    gpg --fast-import cd/signingkey.asc
+    openssl aes-256-cbc -K $encrypted_7786718ab80b_key -iv $encrypted_7786718ab80b_iv -in cd/sk.asc.enc -out cd/sk.asc -d
+    gpg --fast-import cd/sk.asc
 fi
