6e6ae142728ad800300c7f34be7660c181fb5e1e
Chase <chaseadams123@gmail.com>
2017-02-14 18:55:26 -0700

    fixes for travis

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -ev
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_7786718ab80b_key -iv $encrypted_7786718ab80b_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
-    gpg --fast-import cd/codesigning.asc
+    gpg --fast-import cd/signingkey.asc
 fi
